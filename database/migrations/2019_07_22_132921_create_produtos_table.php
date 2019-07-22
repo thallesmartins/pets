5776d8be395e243e64d457e0fc81c86318b65256
Thalles <thallesmartins2@gmail.com>
2019-07-22 11:12:38 -0300

    Treinando

---
+++ database/migrations/2019_07_22_132921_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->double('valor',6,2);
+            $table->string('imagem');
+            $table->enum('ativo',['S','N']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
---
+++ database/migrations/2019_07_22_134133_create_pedido_produtos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidoProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedido_produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('pedido_id');
+            $table->unsignedInteger('produto_id');
+            $table->enum('status',['S','N']);
+            $table->double('valor',6,2);
+            $table->timestamps();
+
+            $table->foreign('pedido_id')->references('id')->on('pedidos')->onDelete('cascade');
+            $table->foreign('produto_id')->references('id')->on('produtos')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_produtos');
+    }
+}
